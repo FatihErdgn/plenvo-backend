d7ac7f5ad38c24c2704a5c82abc9968f6fc94296
FatihErdgn <fatiherdogann099@gmail.com>
2025-02-15 02:29:49 +0300

    sms cron job configured to 5 minutes

--- backend/jobs/reminderJob.js
+++ backend/jobs/reminderJob.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const { sendReminders } = require('../controllers/reminderController');
 
-cron.schedule('*/10 * * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
   console.log('Cron Job: SMS gönderimi başlıyor...');
   await sendReminders();
 });
