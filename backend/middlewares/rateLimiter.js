faa7c2da2f83c920fc56ca3295306ff15b769e80
FatihErdgn <fatiherdogann099@gmail.com>
2025-04-24 23:54:27 +0300

    ratelimiter maxlimit increased

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 dakika
-    max: 1000, // 15 dakika içinde 100 istek
+    max: 10000, // 15 dakika içinde 100 istek
     message: 'Çok fazla istek gönderildi. Lütfen daha sonra tekrar deneyin.',
 });
 
